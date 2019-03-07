607a0391fdbd981c6590261ea38d60e4eab4d4ad
bhyang <brianhyang@berkeley.edu>
2019-03-06 16:54:19 -0800

    adding basic functionality, have not tested yet

--- gym_replab/envs/replab_env.py
+++ gym_replab/envs/replab_env.py
@@ -1,15 +1,29 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+
+
+from controller import *
+import numpy as np
+from config import *
+import rospy
+
+
 class ReplabEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):
-        pass
+    def __init__(self, boundaries=True):
+        rospy.init_node("widowx-custom_controller")
+        self.widowx = WidowX(boundaries)
 
-    def _step(self, action):
+
+    def step(self, action):
         """
 
         Parameters
         ----------
-        action :
+        action : [change in x, change in y, change in z]
 
         Returns
         -------
@@ -33,27 +47,41 @@ class ReplabEnv(gym.Env):
                  However, official evaluations of your agent are not allowed to
                  use this for learning.
         """
-        self._take_action(action)
-        self.status = self.env.step()
-        reward = self._get_reward()
-        ob = self.env.getState()
-        episode_over = self.status != hfo_py.IN_GAME
+        current_pos = self._get_state()
+
+        goal = list(map(add, action, current_pos))
+
+        self._take_action(goal)
+
+        reward = self._get_reward(goal)
+        ob = self._get_state()
+
+        episode_over = False
         return ob, reward, episode_over, {}
 
-    def _reset(self):
+    def reset(self):
+        self.widowx.move_to_neutral()
+
+    def render(self, mode='human', close=False):
         pass
 
-    def _render(self, mode='human', close=False):
+    def close(self):
         pass
 
     def _take_action(self, action):
+        action.extend([-0.008369006790373335, -0.008369006790373335])
+        self.widowx.move_to_target(action)
+        pass
+        
+    def _get_goal_status(self):
         pass
 
-    def _get_reward(self):
-        """ Reward is given for XY. """
-        if self.status == FOOBAR:
-            return 1
-        elif self.status == ABC:
-            return self.somestate ** 2
-        else:
-            return 0
+    def _get_reward(self, goal):
+        """ Reward is negative L2 distance from objective, squared """
+        return - (np.linalg.norm(np.array(goal) - np.array(self._get_state()))**2)
+
+    def _get_state(self):
+        """
+        get_current_pose returns obj with pose(with acctributes posiiton, orientation)
+        """
+        return self.widowx.get_current_pose().pose.position
