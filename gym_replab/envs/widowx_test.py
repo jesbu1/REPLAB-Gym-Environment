124359f2d9672b6d5f4bd8f40687bba76e90ba7b
Jesse Zhang <jessezhang5@gmail.com>
2019-03-11 18:07:28 -0700

    started working on table

--- gym_replab/envs/widowx_test.py
+++ gym_replab/envs/widowx_test.py
@@ -2,14 +2,19 @@ import pybullet as p
 import time
 
 p.connect(p.GUI)
-path = "/Volumes/64GB/RAIL/bullet3/data/"
-p.loadURDF(path + "table/table.urdf", 0.5000000,0.00000,-.820000,0.000000,0.000000,0.0,1.0)
+#path = "/Volumes/64GB/RAIL/bullet3/data/"
+#path = '../../..//bullet3/data/'
+path = "./"
+p.loadURDF(path + "table/table.urdf",[0, 0, 0],[0.000000,0.000000,0.0,1])
 p.setGravity(0,0,-10)
 arm = p.loadURDF(path + "/widowx/widowx.urdf",useFixedBase=True)
 
-p.resetBasePositionAndOrientation(arm,[-0.098612,-0.000726,-0.194018],[0.000000,0.000000,0.000000,1.000000])
+p.resetBasePositionAndOrientation(arm,[0,0,0.58],[0.000000,0.000000,0.000000,-1.000000])
   
-	
+#cuid = p.createCollisionShape(p.GEOM_BOX, halfExtents = [0.5, 0.5, 0.055])
+#mass= 0 #static box
+#p.createMultiBody(mass,cuid)
+#p.resetBasePositionAndOrientation(cuid, [0, 0, -0.194018 - 0.055],[0, 0, 0, 0])
 while (1):
 	p.stepSimulation()
 	time.sleep(0.01)
